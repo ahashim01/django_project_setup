f1a25c712b6a10cf9b67b10c06efc152069012e9
Ahmed Hashim <aahashim01@gmail.com>
2021-03-25 20:44:24 +0200

    python decouple added

--- demo/settings/base.py
+++ demo/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3*e32oz1(c1-ggt%g%+ps4q+qnt+za#2f1(jd!eso#1vjxfv-4'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # Application definition
